fc8e3dfc5d10bde26a730d6211b5152a3e2928c6
Helge Stegemoen <helge@stegemoen.com>
2025-08-08 13:04:03 +0200

    Added 2 custom webroots

--- StaticFilesExample/Program.cs
+++ StaticFilesExample/Program.cs
@@ -1,6 +1,27 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions()
+{
+    WebRootPath = "myroot"
+}); // ToDo
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles(); // works with the web root path (myroot)
+app.UseStaticFiles(new StaticFileOptions()
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath ,"mywebroot")
+        )
+});
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.Map("/", async context =>
+    {
+        await context.Response.WriteAsync("Hello");
+    });
+});
 
 app.Run();
